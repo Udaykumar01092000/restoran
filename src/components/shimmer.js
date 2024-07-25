2ff5c619af11c425016b1364c9b0408551ba68b2
Uday Kumar <udaykumar.77348@gmail.com>
2024-07-25 19:33:10 +0530

    Your commit message

--- src/components/shimmer.js
+++ src/components/shimmer.js
@@ -2,154 +2,31 @@ import React from 'react';
 import '../App.css'
 
 function Shimmer() {
+  const shimmerItems = Array.from({ length: 8 });
+
   return (
-      <div className="row row-cols-1 row-cols-md-4 g-4 pt-4">
-          <div className="col">
-            <div className="card custom-card" aria-hidden="true">
-              <img src="..." className="placeholder card-img-top" alt="..." />
-              <div className="card-body">
-                <h5 className="card-title placeholder-glow">
-                  <span className="placeholder col-12"></span>
-                </h5>
-                <p className="card-text placeholder-glow">
-                  <span className="placeholder col-12"></span>
-                  <span className="placeholder col-12"></span>
-                  <span className="placeholder col-12"></span>
-                  <span className="placeholder col-12"></span>
-                  <span className="placeholder col-12"></span>
-                </p>
-                <button className="btn btn-primary disabled placeholder col-6" aria-disabled="true"></button>
-              </div>
+    <div className="row row-cols-1 row-cols-md-4 g-4 pt-4">
+      {shimmerItems.map((_, index) => (
+        <div className="col" key={index}>
+          <div className="card custom-card" aria-hidden="true">
+            <div className="placeholder card-img-top" alt="" />
+            <div className="card-body">
+              <h5 className="card-title placeholder-glow">
+                <span className="placeholder col-12"></span>
+              </h5>
+              <p className="card-text placeholder-glow">
+                <span className="placeholder col-12"></span>
+                <span className="placeholder col-12"></span>
+                <span className="placeholder col-12"></span>
+                <span className="placeholder col-12"></span>
+                <span className="placeholder col-12"></span>
+              </p>
+              <button className="btn btn-primary disabled placeholder col-6" aria-disabled="true"></button>
             </div>
           </div>
-          <div className="col">
-            <div className="card custom-card" aria-hidden="true">
-              <img src="..." className="placeholder card-img-top" alt="..." />
-              <div className="card-body">
-                <h5 className="card-title placeholder-glow">
-                  <span className="placeholder col-12"></span>
-                </h5>
-                <p className="card-text placeholder-glow">
-                  <span className="placeholder col-12"></span>
-                  <span className="placeholder col-12"></span>
-                  <span className="placeholder col-12"></span>
-                  <span className="placeholder col-12"></span>
-                  <span className="placeholder col-12"></span>
-                </p>
-                <button className="btn btn-primary disabled placeholder col-6" aria-disabled="true"></button>
-              </div>
-            </div>
-          </div>
-          <div className="col">
-            <div className="card custom-card" aria-hidden="true">
-              <img src="..." className="placeholder card-img-top" alt="..." />
-              <div className="card-body">
-                <h5 className="card-title placeholder-glow">
-                  <span className="placeholder col-12"></span>
-                </h5>
-                <p className="card-text placeholder-glow">
-                  <span className="placeholder col-12"></span>
-                  <span className="placeholder col-12"></span>
-                  <span className="placeholder col-12"></span>
-                  <span className="placeholder col-12"></span>
-                  <span className="placeholder col-12"></span>
-                </p>
-                <button className="btn btn-primary disabled placeholder col-6" aria-disabled="true"></button>
-              </div>
-            </div>
-          </div>
-          <div className="col">
-            <div className="card custom-card" aria-hidden="true">
-              <img className="placeholder card-img-top" alt="..." />
-              <div className="card-body">
-                <h5 className="card-title placeholder-glow">
-                  <span className="placeholder col-12"></span>
-                </h5>
-                <p className="card-text placeholder-glow">
-                  <span className="placeholder col-12"></span>
-                  <span className="placeholder col-12"></span>
-                  <span className="placeholder col-12"></span>
-                  <span className="placeholder col-12"></span>
-                  <span className="placeholder col-12"></span>
-                </p>
-                <button className="btn btn-primary disabled placeholder col-6" aria-disabled="true"></button>
-              </div>
-            </div>
-          </div>
-          <div className="col">
-            <div className="card custom-card" aria-hidden="true">
-              <img className="placeholder card-img-top" alt="..." />
-              <div className="card-body">
-                <h5 className="card-title placeholder-glow">
-                  <span className="placeholder col-12"></span>
-                </h5>
-                <p className="card-text placeholder-glow">
-                  <span className="placeholder col-12"></span>
-                  <span className="placeholder col-12"></span>
-                  <span className="placeholder col-12"></span>
-                  <span className="placeholder col-12"></span>
-                  <span className="placeholder col-12"></span>
-                </p>
-                <button className="btn btn-primary disabled placeholder col-6" aria-disabled="true"></button>
-              </div>
-            </div>
-          </div>
-          <div className="col">
-            <div className="card custom-card" aria-hidden="true">
-              <img className="placeholder card-img-top" alt="..." />
-              <div className="card-body">
-                <h5 className="card-title placeholder-glow">
-                  <span className="placeholder col-12"></span>
-                </h5>
-                <p className="card-text placeholder-glow">
-                  <span className="placeholder col-12"></span>
-                  <span className="placeholder col-12"></span>
-                  <span className="placeholder col-12"></span>
-                  <span className="placeholder col-12"></span>
-                  <span className="placeholder col-12"></span>
-                </p>
-                <button className="btn btn-primary disabled placeholder col-6" aria-disabled="true"></button>
-              </div>
-            </div>
-          </div>
-          <div className="col">
-            <div className="card custom-card" aria-hidden="true">
-              <img className="placeholder card-img-top" alt="..." />
-              <div className="card-body">
-                <h5 className="card-title placeholder-glow">
-                  <span className="placeholder col-12"></span>
-                </h5>
-                <p className="card-text placeholder-glow">
-                  <span className="placeholder col-12"></span>
-                  <span className="placeholder col-12"></span>
-                  <span className="placeholder col-12"></span>
-                  <span className="placeholder col-12"></span>
-                  <span className="placeholder col-12"></span>
-                </p>
-                <button className="btn btn-primary disabled placeholder col-6" aria-disabled="true"></button>
-              </div>
-            </div>
-          </div>
-          <div className="col">
-            <div className="card custom-card" aria-hidden="true">
-              <img className="placeholder card-img-top" alt="..." />
-              <div className="card-body">
-                <h5 className="card-title placeholder-glow">
-                  <span className="placeholder col-12"></span>
-                </h5>
-                <p className="card-text placeholder-glow">
-                  <span className="placeholder col-12"></span>
-                  <span className="placeholder col-12"></span>
-                  <span className="placeholder col-12"></span>
-                  <span className="placeholder col-12"></span>
-                  <span className="placeholder col-12"></span>
-                </p>
-                <button className="btn btn-primary disabled placeholder col-6" aria-disabled="true"></button>
-              </div>
-            </div>
-          </div>
-      </div>
-      
+        </div>
+      ))}
+    </div>
   );
 }
 
